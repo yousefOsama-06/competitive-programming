6c3422d8793c861e25986a1f18d46953eccfc219
yousefOsama-06 <yousof.osama.2006@gmail.com>
2026-08-10 23:06:29 +0300

    refactor: remove built-in GCD_LCM file and renumber ExtendedEuclid

--- notebook/04 - Number Theory/01 - Euclidean Algorithm/01 - GCD_LCM.cpp
+++
@@ -1,9 +0,0 @@
-// GCD & LCM - Standard C++17 Built-ins (#include <numeric>)
-// std::gcd(a, b) and std::lcm(a, b) are available natively in C++17
-// Time Complexity: O(log(min(a, b)))
-
-#include <numeric>
-
-// Standard C++17 built-ins:
-// ll g = std::gcd(a, b);
-// ll l = std::lcm(a, b);
