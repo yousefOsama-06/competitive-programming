e1aecd8e87bf333fa2612345535a7e34f483f60e
yousefOsama-06 <yousof.osama.2006@gmail.com>
2026-08-10 23:03:02 +0300

    refactor: encapsulate Sieve into struct Sieve

--- notebook/04 - Number Theory/02 - Primes and Divisors/01 - Sieve.cpp
+++ notebook/04 - Number Theory/02 - Primes and Divisors/01 - Sieve.cpp
@@ -1,32 +1,34 @@
 // Linear Sieve & Smallest Prime Factor (SPF) - Time: O(N), Space: O(N)
 // Computes primes list, SPF array, primality test, and O(log N) prime factorization
-const int MAXN = 1e7;
-vector<int> primes;
-int spf[MAXN + 1];
+struct Sieve {
+    int n;
+    vector<int> primes;
+    vector<int> spf;
 
-void sieve(int n = MAXN) {
-    for (int i = 2; i <= n; i++) {
-        if (spf[i] == 0) {
-            spf[i] = i;
-            primes.pb(i);
-        }
-        for (int p : primes) {
-            if (p > spf[i] || (ll)i * p > n) break;
-            spf[i * p] = p;
+    Sieve(int n = 1e7) : n(n), spf(n + 1, 0) {
+        for (int i = 2; i <= n; i++) {
+            if (spf[i] == 0) {
+                spf[i] = i;
+                primes.pb(i);
+            }
+            for (int p : primes) {
+                if (p > spf[i] || (ll)i * p > n) break;
+                spf[i * p] = p;
+            }
         }
     }
-}
 
-bool is_prime(int x) {
-    return x >= 2 && spf[x] == x;
-}
-
-// O(log N) prime factorization using precomputed SPF array
-vector<int> get_prime_factors(int n) {
-    vector<int> factors;
-    while (n > 1) {
-        factors.pb(spf[n]);
-        n /= spf[n];
+    bool is_prime(int x) const {
+        return x >= 2 && x <= n && spf[x] == x;
     }
-    return factors;
-}
+
+    // O(log N) prime factorization using precomputed SPF array
+    vector<int> get_prime_factors(int x) const {
+        vector<int> factors;
+        while (x > 1) {
+            factors.pb(spf[x]);
+            x /= spf[x];
+        }
+        return factors;
+    }
+};
