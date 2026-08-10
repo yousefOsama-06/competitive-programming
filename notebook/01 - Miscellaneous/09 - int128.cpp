465b95cc157a99341ff8137411e0ef65a7644910
yousefOsama-06 <yousof.osama.2006@gmail.com>
2026-08-10 23:21:20 +0300

    refactor: simplify int128 template to stream operators operator<< and operator>>

--- notebook/01 - Miscellaneous/09 - int128.cpp
+++ notebook/01 - Miscellaneous/09 - int128.cpp
@@ -1,19 +1,6 @@
-// __int128 I/O & Stream Operators Helper
+// __int128 Stream Operators Helper
 typedef __int128_t int128;
 typedef __uint128_t uint128;
-
-int128 read128() {
-    int128 x = 0; int f = 1; char ch = getchar();
-    while (ch < '0' || ch > '9') { if (ch == '-') f = -1; ch = getchar(); }
-    while (ch >= '0' && ch <= '9') { x = x * 10 + ch - '0'; ch = getchar(); }
-    return x * f;
-}
-
-void print128(int128 x) {
-    if (x < 0) { putchar('-'); x = -x; }
-    if (x > 9) print128(x / 10);
-    putchar(x % 10 + '0');
-}
 
 ostream &operator<<(ostream &os, int128 n) {
     if (n == 0) return os << "0";
