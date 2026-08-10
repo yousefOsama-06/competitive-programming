8176d48a9028e064fa454d39779f7c6e6c809fa1
yousefOsama-06 <yousof.osama.2006@gmail.com>
2026-08-10 23:03:48 +0300

    refactor: encapsulate all Primes and Divisors templates into structs

--- notebook/04 - Number Theory/02 - Primes and Divisors/02 - Factorization.cpp
+++ notebook/04 - Number Theory/02 - Primes and Divisors/02 - Factorization.cpp
@@ -1,13 +1,15 @@
-template <typename T>
-vector<T> getPrimeFactors(T n) {
-    //complexity: sqrt(n)
-    vector<T> vec;
-    for (ll i = 2; i * i <= n; i++) {
-        while (n % i == 0) {
-            vec.push_back(i);
-            n /= i;
+// Trial Division Prime Factorization - Time: O(sqrt(N))
+template <typename T = ll>
+struct Factorization {
+    static vector<T> get_prime_factors(T n) {
+        vector<T> factors;
+        for (T i = 2; i * i <= n; i++) {
+            while (n % i == 0) {
+                factors.pb(i);
+                n /= i;
+            }
         }
+        if (n > 1) factors.pb(n);
+        return factors;
     }
-    if (n != 1) vec.push_back(n);
-    return vec;
-}
+};
--- notebook/04 - Number Theory/02 - Primes and Divisors/03 - Divisors.cpp
+++ notebook/04 - Number Theory/02 - Primes and Divisors/03 - Divisors.cpp
@@ -1,13 +1,14 @@
-template <typename T>
-vector<T> getDivisors(T n) {
-    //complexity: sqrt(n)
-    vector<T> vec;
-    for (ll i = 1; i * i <= n; i++) {
-        if (n % i == 0) {
-            vec.push_back(i);
-            if (n / i != i)
-                vec.push_back(n / i);
+// Trial Division Divisor Generation - Time: O(sqrt(N))
+template <typename T = ll>
+struct Divisors {
+    static vector<T> get_divisors(T n) {
+        vector<T> divs;
+        for (T i = 1; i * i <= n; i++) {
+            if (n % i == 0) {
+                divs.pb(i);
+                if (n / i != i) divs.pb(n / i);
+            }
         }
+        return divs;
     }
-    return vec;
-}
+};
