222f6ed395789e9bbb574d65ada98df5bc173928
yousefOsama-06 <yousof.osama.2006@gmail.com>
2026-08-10 23:11:21 +0300

    feat: add Segmented Sieve for Range [L, R] template

---
+++ notebook/04 - Number Theory/02 - Primes and Divisors/05 - SegmentedSieve.cpp
@@ -0,0 +1,30 @@
+// Segmented Sieve for Range [L, R] - Time: O(sqrt(R) + (R - L) log log R), Space: O(sqrt(R) + R - L)
+// Computes all prime numbers in range [L, R] where R <= 1e12 and (R - L) <= 1e7
+struct SegmentedSieve {
+    static vector<ll> get_primes_in_range(ll L, ll R) {
+        ll limit = sqrt(R);
+        vector<bool> is_prime_small(limit + 1, true);
+        vector<ll> primes;
+        for (ll i = 2; i <= limit; i++) {
+            if (is_prime_small[i]) {
+                primes.pb(i);
+                for (ll j = i * i; j <= limit; j += i) is_prime_small[j] = false;
+            }
+        }
+
+        vector<bool> is_prime_range(R - L + 1, true);
+        for (ll p : primes) {
+            ll start = max(p * p, (L + p - 1) / p * p);
+            for (ll j = start; j <= R; j += p) {
+                is_prime_range[j - L] = false;
+            }
+        }
+        if (L == 1) is_prime_range[0] = false;
+
+        vector<ll> result;
+        for (ll i = 0; i <= R - L; i++) {
+            if (is_prime_range[i]) result.pb(L + i);
+        }
+        return result;
+    }
+};
