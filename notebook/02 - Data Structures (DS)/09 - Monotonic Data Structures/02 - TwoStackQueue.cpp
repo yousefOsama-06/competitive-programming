6849e55d89a97500613fb5e97b2ffcac4d00b68d
yousefOsama-06 <yousof.osama.2006@gmail.com>
2026-08-10 22:54:14 +0300

    docs: add explanatory comments and complexity header to TwoStackQueue

--- notebook/02 - Data Structures (DS)/09 - Monotonic Data Structures/02 - TwoStackQueue.cpp
+++ notebook/02 - Data Structures (DS)/09 - Monotonic Data Structures/02 - TwoStackQueue.cpp
@@ -1,37 +1,42 @@
+// Monotonic Queue via Two Stacks (Sliding Window Min/Max)
+// Space: O(N), Time: O(1) amortized per push/pop/get
 struct TwoStackQ {
     struct Node {
         ll mx = -llinf, mn = llinf, val;
 
-        Node(): val(0) {
-        }
-
-        Node(ll x): mx(x), mn(x), val(x) {
-        }
+        Node() : val(0) {}
+        Node(ll x) : mx(x), mn(x), val(x) {}
     };
 
     stack<Node> a, b;
 
-    int size() { return a.size() + b.size(); }
+    int size() {
+        return a.size() + b.size();
+    }
 
-    void mrg(Node& a, Node& b) {
+    void mrg(Node &a, Node &b) {
         a.mn = min(a.mn, b.mn);
         a.mx = max(a.mx, b.mx);
     }
 
+    // Pushes value into the queue
     void push(ll val) {
         auto nd = Node(val);
         if (!a.empty()) mrg(nd, a.top());
         a.push(nd);
     }
 
+    // Transfers elements from stack a to stack b when b is empty
     void move() {
         while (!a.empty()) {
             auto nd = Node(a.top().val);
             if (!b.empty()) mrg(nd, b.top());
-            b.push(nd), a.pop();
+            b.push(nd);
+            a.pop();
         }
     }
 
+    // Returns min/max of all current elements in the queue in O(1)
     Node get() {
         Node res;
         if (!b.empty()) mrg(res, b.top());
@@ -39,6 +44,7 @@ struct TwoStackQ {
         return res;
     }
 
+    // Removes the oldest element from the queue
     void pop() {
         if (b.empty()) move();
         if (!b.empty()) b.pop();
